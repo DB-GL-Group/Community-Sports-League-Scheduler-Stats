838c46b5a67c18c14d6ca5bf5c3261a9c44fd5d2
zedbros <jameszed@icloud.com>
2026-01-11 05:58:59 +0100

    Tiny error fix + match/case

--- backend/worker/tasks/scheduler.py
+++ backend/worker/tasks/scheduler.py
@@ -27,28 +27,29 @@ async def _run_scheduler_job(job_id: str) -> None:
     await open_async_pool()
     try:
         # TODO : Implement scheduler logic
-        if job_id == 1:
-            venue_id = add_venue("Rocks The Lakes", "Rue de Saint-Pierre 12")
-            court1_id = add_court(venue_id, "Court1", "IDK what surface is lmao")
-            slot_id = add_slot(court1_id, time.time(), time.time()+36000)
+        match job_id:
+            case "1":
+                venue_id = add_venue("Rocks The Lakes", "Rue de Saint-Pierre 12")
+                court1_id = add_court(venue_id, "Court1", "IDK what surface is lmao")
+                slot_id = add_slot(court1_id, time.time(), time.time()+36000)
 
-            JohnDoeManager_id = create_manager("John", "Doe", "johndoe@gmail.com", "+41 123 123 12")
-            home_team_id = create_team("The flightless sharks", JohnDoeManager_id, "FLS", "Blue", "White")
-            player1 = create_player("Mark", "Evans", "markevans@gmail.com", "+41 890 890 90")
-            add_player(player1, home_team_id)
+                JohnDoeManager_id = create_manager("John", "Doe", "johndoe@gmail.com", "+41 123 123 12")
+                home_team_id = create_team("The flightless sharks", JohnDoeManager_id, "FLS", "Blue", "White")
+                player1 = create_player("Mark", "Evans", "markevans@gmail.com", "+41 890 890 90")
+                add_player(player1, home_team_id)
 
-            JaneDoughManager_id = create_manager("Jane", "Dough", "janedouch@gmail.com", "+41 321 321 32")
-            away_team_id = create_team("The Thirsty Fish", JaneDoughManager_id, "TTF", "Purple", "Yellow")
-            player2 = create_player("Axel", "Blaze", "axelblaze@gmail.com", "+41 098 098 09")
-            add_player(player2, away_team_id)
+                JaneDoughManager_id = create_manager("Jane", "Dough", "janedouch@gmail.com", "+41 321 321 32")
+                away_team_id = create_team("The Thirsty Fish", JaneDoughManager_id, "TTF", "Purple", "Yellow")
+                player2 = create_player("Axel", "Blaze", "axelblaze@gmail.com", "+41 098 098 09")
+                add_player(player2, away_team_id)
 
-            ref_id = create_referee("Reeses", "Puffs", "reesespuffs@gmail.com", "+12 456 456 45")
-            match1 = add_match(slot_id, home_team_id, away_team_id, ref_id, "Active")
+                ref_id = create_referee("Reeses", "Puffs", "reesespuffs@gmail.com", "+12 456 456 45")
+                match1 = add_match(slot_id, home_team_id, away_team_id, ref_id, "Active")
 
-            print(get_all_matches())
-            print("This is the end of ze match my friend.")
-        else:
-            print("YOUUUUUU SKIPPED YOUR JOB.. NO PAY FOR YOU MA MAN !!")
+                print(get_all_matches())
+                print("This is the end of ze match my friend.")
+            case _:
+                print("YOUUUUUU SKIPPED YOUR JOB (or just no job_id).. NO PAY FOR YOU MA MAN !!")
         # pass
     finally:
         await close_async_pool()
